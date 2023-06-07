a147fa6d5d4fb6055cf58978c1bf485e0b37ad93
qudwn1114 <qudwn1114@gmail.com>
2023-06-07 20:38:14 +0900

    feat: spring sequrity

---
+++ src/main/java/com/example/test/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration //스프링의 환경설정 파일임을 의미
+@EnableWebSecurity //모든 요청 URL이 스프링 시큐리티의 제어
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+            .and().csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/**"));
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/com/example/test/config/webMvcConfig.java
+++ src/main/java/com/example/test/config/webMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class webMvcConfig implements WebMvcConfigurer{
+public class WebMvcConfig implements WebMvcConfigurer{
     private String resourcePath = "/media/**"; // view 에서 접근할 경로
     private String savePath = "file://" + System.getProperty("user.dir") + "/media/"; // 실제 파일저장 경로
     
